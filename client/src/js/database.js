1b2b24841af6fba0fd417aab2bc66ab9d8388f5d
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-09 12:37:23 -0700

    updated package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,12 +1,15 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+  // We are creating a new database named 'jate' which will be using version 1 of the database.
   openDB('jate', 1, {
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
@@ -16,6 +19,25 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
